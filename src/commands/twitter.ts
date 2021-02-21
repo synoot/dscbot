3c54a663d3f73598ae4f956a806c84b027cfceb0
synoot <xxsonic4xx@gmail.com>
2021-02-20 22:22:41 -0800

    amogus.ts -> twitter.ts | -amogus -> -tweet

--- src/commands/twitter.ts
+++ src/commands/twitter.ts
@@ -3,9 +3,10 @@ import { Command } from "../commandhandler";
 import { FileHelper } from "../filehelper";
 import Twitter from "twitter";
 import { CommandResponse, TypeObject } from "../types";
+import { getPrefix } from "..";
 
 const helper = new FileHelper()
-const config = helper.readFile("./save/amogus-config.json")
+const config = helper.readFile("./save/twitter-config.json")
 
 const twitter = new Twitter({
     consumer_key: config.api_key,
@@ -14,25 +15,22 @@ const twitter = new Twitter({
 })
 
 exports.command = new Command({
-    name: "amogus",
-    description: "Fetches the latest tweet from @sustoss on Twitter",
-    category: "Amogus",
+    name: "tweet",
+    description: "Fetches the latest tweet from a given Twitter user",
+    category: "Twitter",
     async callback(msg : Message) {
-        let ret : TypeObject<any> = {
-            title: "Error?",
-            description: "Error!"
-        }
+        const username = (msg.content.substr((this.prefix || getPrefix()).length + this.name.length + 1)).toLowerCase()
 
-        let prom : Promise<CommandResponse> = twitter.get("statuses/user_timeline", {screen_name: config.userid, count: 1}).then((tweets : Twitter.ResponseData) => {
+        let prom : Promise<CommandResponse> = twitter.get("statuses/user_timeline", {screen_name: username, count: 1}).then((tweets : Twitter.ResponseData) => {
             const tweet = tweets[0]
             
             let embed : TypeObject<any> = {
                 color: 0x1D9BF0,
                 title: "Tweet Link",
-                url: `https://www.twitter.com/sustoss/status/${tweet.id_str}`,
+                url: `https://www.twitter.com/${username}/status/${tweet.id_str}`,
                 author: {
-                    name: "sustoss",
-                    url: "https://www.twitter.com/sustoss",
+                    name: username,
+                    url: `https://www.twitter.com/${username}`,
                     icon_url: tweet.user.profile_image_url_https
                 },
                 timestamp: new Date(tweet.created_at),
@@ -55,13 +53,25 @@ exports.command = new Command({
                         embed: embed
                     })
                 } else {
-                    reject("No tweets?")
+                    reject({
+                        isReply: false,
+                        message: "Error."
+                    })
                 }
             })
 
             return promise as Promise<CommandResponse>
         }).catch((err) => {
-            throw err;
+            console.error(err)
+            
+            const promise = new Promise(function(resolve) {
+                resolve({
+                    isReply: false,
+                    message: "Couldn't get any Tweets. Common reasons include private accounts, suspended accounts, or the account itself does not exist."
+                })
+            })
+
+            return promise as Promise<CommandResponse>
         })
 
         return prom
